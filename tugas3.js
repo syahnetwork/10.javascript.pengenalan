3c64c9015af41fe7796251371612664e8c35b010
Adam <syahnetwork@gmail.com>
2020-04-13 03:40:40 +0700

    tugas3

--- tugas3.js
+++ tugas3.js
@@ -6,8 +6,21 @@ let function2 = (str = 'adam') => {
   return console.log(str + ' likes to watch forge in fire')
 }
 
+function es5Function(favoriteSword = 'zhanmadao') {
+  return console.log(favoriteSword)
+}
+
+let es5FunctionVariable = function es5Function(favoriteSword = 'zhanmadao') {
+  return console.log(favoriteSword)
+}
+
 function1()
 function1(5)
 
 function2()
 function2('borat')
+
+es5Function()
+es5Function('javanese kris')
+es5FunctionVariable('flamberge')
+es5FunctionVariable('zweihander')
