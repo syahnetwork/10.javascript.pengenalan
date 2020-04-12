1367c28e25afcc0f0805bf78d8d6441e5ecd7024
Adam <syahnetwork@gmail.com>
2020-04-13 02:04:53 +0700

    tugas2

---
+++ tugas2.js
@@ -0,0 +1,39 @@
+let number1 = 1
+let number2 = 2
+let number3 = 3
+let number4 = 4
+let number5 = 5
+
+console.log(number1, number2, number3, number4, number5)
+
+let string1 = 'nkri1'
+let string2 = 'nkri2'
+let string3 = 'nkri3'
+let string4 = 'nkri4'
+let string5 = 'nkri5'
+
+console.log(string1, string2, string3, string4, string5)
+
+let boolean1 = true
+let boolean2 = false
+let boolean3 = true
+let boolean4 = false
+let boolean5 = true
+
+console.log(boolean1, boolean2, boolean3, boolean4, boolean5)
+
+let nullVariable = null
+console.log(nullVariable)
+
+let undefinedVariable
+console.log(undefinedVariable)
+
+let arrayVariable1 = [10, 20, 30, 40]
+console.log(arrayVariable1[2])
+console.log(arrayVariable1[1])
+console.log(arrayVariable1[1] - arrayVariable1[3])
+
+
+let arrayVariable2 = ['forged', 'in', 'fire', 'awesome']
+console.log(arrayVariable2[1] + arrayVariable2[2])
+console.log(arrayVariable2[3])
