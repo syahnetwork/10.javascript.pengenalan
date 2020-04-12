c3dd21b406d80f5a15c9831cbdb0587ccdcf2ef9
Adam <syahnetwork@gmail.com>
2020-04-13 01:52:24 +0700

    tugas 1

---
+++ tugas1.js
@@ -0,0 +1,15 @@
+var firstName1 = 'adam'
+var firstName2 = 'beta'
+var firstName3 = 'cash'
+
+let lastName1 = 'nkri'
+let lastName2 = 'fox'
+let lastName3 = 'sharp'
+
+const age1 = 22
+const age2 = 23
+const age3 = 24
+
+console.log(firstName1 + ' ' + lastName1 + ' age ' + age1)
+console.log(firstName2 + ' ' + lastName2 + ' age ' + age2)
+console.log(firstName3 + ' ' + lastName3 + ' age ' + age3)
