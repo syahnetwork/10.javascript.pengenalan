228e8ef5ff230b572ee414e0ca4ed38ac58e78d9
Adam <syahnetwork@gmail.com>
2020-04-13 11:31:34 +0700

    tugas8

---
+++ tugas8.js
@@ -0,0 +1,12 @@
+let motor = {
+  merk: 'rx-king',
+  harga: 5000000,
+  warna: 'hitam',
+  produksi: true
+}
+
+console.log(motor)
+console.log(motor.merk)
+console.log('==========')
+motor.pemilik = 'adam'
+console.log(motor)
